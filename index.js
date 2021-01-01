14fcd46f3148349b8a851c27ea7bc04f41faf209
jcastro506 <castrojoshua805.com>
2021-01-01 15:13:53 -0500

    commit tree f262fd9fba980a0f03f05e2f8d36a8f0d7880bd4

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
-// Your code here
+
+function mapToNegativize(sourceArray){
+    return sourceArray.map(x => x * -1)
+} 
+
+const mapToNoChange = sourceArray => {
+    return sourceArray.map(number => number)
+}
+
+function reduceToTotal(sourceArray, starter){
+    return sourceArray.reduce(number => number + number)
+} 
